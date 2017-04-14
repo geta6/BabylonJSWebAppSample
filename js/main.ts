5e1671d03a700c5367422183bfec97c98dcd2868
mhawker <michael.hawker@outlook.com>
2017-04-14 13:17:52 -0700

    Add BabylonJS Quick Start Example Add Windows FullScreen App Call Example

---
+++ js/main.ts
@@ -0,0 +1,15 @@
+﻿
+window.addEventListener("DOMContentLoaded", () => {
+    // make window full screen
+    Windows.UI.ViewManagement.ApplicationView.getForCurrentView().tryEnterFullScreenMode();
+    Windows.UI.ViewManagement.ApplicationView.preferredLaunchWindowingMode = Windows.UI.ViewManagement.ApplicationViewWindowingMode.fullScreen;
+
+    // create the app using the 'renderCanvas'
+    let app = new BabylonApp("renderCanvas");
+
+    // create the scene
+    app.createScene();
+
+    // start animation
+    app.animate();
+});
